07b385fabc7cf9d4c3704e2801cdc835575bea00
qezhhnjy <fzyszsz@163.com>
2022-04-17 02:32:17 +0800

    添加IM框

--- src/pages/account/Settings/index.tsx
+++ src/pages/account/Settings/index.tsx
@@ -1,14 +1,20 @@
-import { PageContainer } from '@ant-design/pro-layout';
+import {PageContainer} from '@ant-design/pro-layout';
 import React from 'react';
 import {useModel} from "@@/plugin-model/useModel";
+import {Col, Row} from 'antd';
 
 const AccountSettings: React.FC = () => {
-  const { initialState } = useModel('@@initialState');
-  const { currentUser } = initialState || {};
+  const {initialState} = useModel('@@initialState');
+  const {currentUser} = initialState || {};
   return (
     <PageContainer>
       <h1>个人设置</h1>
       <span>{JSON.stringify(currentUser)}</span>
+      <Row style={{width: '60%', height: '600px'}}>
+        <Col flex={60} style={{backgroundColor: 'darkblue'}}/>
+        <Col flex={240}/>
+        <Col flex='auto'/>
+      </Row>
     </PageContainer>
   );
 };
