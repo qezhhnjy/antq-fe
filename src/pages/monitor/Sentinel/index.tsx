dd5a3f6a46718cd6e4bc3a8ec6d3721e20fa415c
qezhhnjy <fzyszsz@163.com>
2022-06-24 21:27:12 +0800

    FatMap

--- src/pages/monitor/Sentinel/index.tsx
+++ src/pages/monitor/Sentinel/index.tsx
@@ -4,8 +4,7 @@ const Sentinel: React.FC<any> = (props) => {
   const {} = props;
   return (
     // <iframe frameBorder={0} style={{width: '100%', height: 830, overflow: 'visible'}} src='http://43.142.106.115:11004'/>
-    <iframe width="560" height="400" frameBorder="0" src="https://fatmap.com/routeid/2982528/?fmid=em"/>
-  )
+    <iframe width="560" height="400" frameBorder="0" src="https://fatmap.com/routeid/2982532/?fmid=em"/>  )
 }
 
 export default Sentinel;
