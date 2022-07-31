d3d934b1ebcf87592fdcb4ca9a8ce42d414e8a64
qezhhnjy <fzyszsz@163.com>
2022-07-31 17:12:40 +0800

    添加图集相册

--- src/locales/zh-CN/menu.ts
+++ src/locales/zh-CN/menu.ts
@@ -84,6 +84,7 @@ export default {
   'menu.robo.market-statistic': '时长统计',
   'menu.outdoor': '户外运动',
   'menu.outdoor.hiking-trail': '徒步路线',
+  'menu.account.emqx': 'emqx',
   'menu.album': '图集相册',
   'menu.album.detail': '图集详情',
   'menu.album.list': '图集列表',
